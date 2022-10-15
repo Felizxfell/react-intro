09d6eb931b0760908b9982c5ffaebfedf475f091
Felizxfell <felizxhgf@gmial.com>
2022-10-15 13:16:09 -0500

    Updates

---
+++ precache-manifest.e2cc4108509dc06b0fbdb832c70b822f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59d1a9a25c379629b844",
+    "url": "/react-intro/static/css/main.1f7e2f17.chunk.css"
+  },
+  {
+    "revision": "59d1a9a25c379629b844",
+    "url": "/react-intro/static/js/main.59d1a9a2.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/react-intro/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "bef8bd6fe6c496820fb4",
+    "url": "/react-intro/static/js/runtime~main.bef8bd6f.js"
+  },
+  {
+    "revision": "0b480a9ba415fcc9e3749f08c9afddc9",
+    "url": "/react-intro/index.html"
+  }
+];
