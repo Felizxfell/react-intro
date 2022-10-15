012f9f561f37ef64d98803b70cff09f9c371dc14
Felizxfell <felizxhgf@gmial.com>
2022-10-15 13:14:19 -0500

    pagina de carga

--- src/components/AppUI.jsx
+++ src/components/AppUI.jsx
@@ -5,6 +5,7 @@ import TodoList from "./TodoList";
 import TodoItem from "./TodoItem";
 import CreateTodoButtom from "./CreateTodoButtom";
 import { TodoContext } from './TodoContext';
+import { TodoLoading, TodosEmpty, TodosError } from './TodoLoading';
 import TodoForm from './TodoForm';
 import Modal from '../Modal';
 
@@ -25,9 +26,9 @@ const AppUI = () =>
             <TodoCounter />
             <TodoSearch />
             <TodoList>            
-                {error && <p>Desespérate, hubo un error...</p>}        
-                {loading && <p>Estamos cargando, no desesperes...</p>}        
-                {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+                {error && <TodosError />} 
+                {loading && <TodoLoading />}
+                {(!loading && !searchedTodos.length) && <TodosEmpty />}
                 {searchedTodos.map((todo) => (
                     <TodoItem
                         key={todo.text}
---
+++ src/components/TodoLoading.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '../css/TodoLoading.css'
+
+const TodoLoading = () => {
+    return (
+        <div className='load-container'>
+            <div className='load-background'>
+                <div className='bar'></div>
+                <div className='mask thick'></div>
+                <div className='bar'></div>
+                <div className='mask thick'></div>
+                <div className='bar'></div>
+                <div className='mask thick'></div>
+                <div className='bar'></div>
+            </div>
+        </div>
+    )
+}
+
+export { TodoLoading };
+
+const TodosEmpty = () => {
+    return (
+        <div className='sindata-container'>
+            <p>¡Crea tu primer TODO!</p>
+        </div>
+    )
+}
+
+export { TodosEmpty };
+
+const TodosError = () => {
+    return (
+        <div className='sindata-container'>
+            <p>Desespérate, hubo un error...</p>
+        </div>
+    )
+}
+
+export { TodosError };
