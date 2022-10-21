e18c425ae9e967f9908416041c0ed99dc02acc0e
FelipeJesus <fisidro@kachimushi.com>
2022-10-21 17:50:29 -0500

    arreglando componentes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,37 @@ import React from 'react'
 import AppUI from './AppUI'
 import { TodoProvider } from './TodoContext';
 
-function App() {
-   return (
+function App()
+{
+  return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
+}
+
+const TodoHeader = ({ children }) => {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+const TodoList = ({ children }) => {
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
+}
+const TodoCounter = () => {
+  return <p>TodoCounter</p>;
+}
+const TodoSearch = () => {
+  return <p>TodoSearch</p>;
+}
+const TodoItem = ({ state }) => {
+  return <p>TodoItem: {state}</p>;
 }
 
 export default App;
