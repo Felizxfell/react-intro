f082a730b0992d5e4c37fe76c9ddd3da3f40f0b3
Felizxfell <felizxhgf@gmial.com>
2022-11-29 10:48:02 -0600

    agrege la libreria react-icon y la implemento

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import '../css/TodoItem.css'
+import { HiOutlineCheck, HiOutlineTrash } from "react-icons/hi";
 
 function TodoItem(props)
 {
     const { completed, text, onComplete, onDelete } = props
     return (
         <li className='item'>
-            <span className={`icon icon-check ${completed ? 'icon-check-active' : 'icon-check-false'}`} onClick={onComplete}>
-                <i className="las la-check"></i>
+            <span className={`icon icon-check ${completed ? 'icon-check-active' : 'icon-check-false'}`} onClick={onComplete}>                
+                <HiOutlineCheck />
             </span>
             <p className={`item-p ${completed && 'item-p-complete'}`}>
                 {text}
             </p>
             <span className='delete' onClick={onDelete}>
-                <i className="las la-trash"></i>
+                <HiOutlineTrash />
             </span>
         </li>
     )
