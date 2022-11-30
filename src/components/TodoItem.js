1ffba091fc9e543b00e21f0a57fcf302c28ee39b
Felizxfell <felizxhgf@gmial.com>
2022-11-29 18:59:52 -0600

    diseño de boton edicion

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import '../css/TodoItem.css'
-import { HiOutlineCheck, HiOutlineTrash } from "react-icons/hi";
+import { HiOutlineCheck, HiOutlinePencil, HiOutlineTrash } from "react-icons/hi";
 
-function TodoItem(props)
-{
-    const { completed, text, onComplete, onDelete } = props
+function TodoItem(props) {
+    const { completed, text, onEdit, onComplete, onDelete } = props
     return (
         <li className='item'>
-            <span className={`icon icon-check ${completed ? 'icon-check-active' : 'icon-check-false'}`} onClick={onComplete}>                
+            <span className={`icon icon-check ${completed ? 'icon-check-active' : 'icon-check-false'}`} onClick={onComplete}>
                 <HiOutlineCheck />
             </span>
             <p className={`item-p ${completed && 'item-p-complete'}`}>
                 {text}
             </p>
+            <span className='edit' onClick={onEdit}>
+                <HiOutlinePencil />
+            </span>
             <span className='delete' onClick={onDelete}>
                 <HiOutlineTrash />
             </span>
